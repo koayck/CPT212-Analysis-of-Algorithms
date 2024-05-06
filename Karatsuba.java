7fbb20b55ba7f213335c5ff8acd2d3cab027359b
Jacky Chung <clashofjacky@email.com>
2024-05-06 15:10:39 +0800

    first checking(without the n=1000)

--- Karatsuba.java
+++ Karatsuba.java
@@ -3,6 +3,24 @@
 // Importing Random class from java.util packahge
 import java.util.Random;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.chart.LineChart;
+// import javafx.scene.chart.NumberAxis;
+// import javafx.scene.chart.XYChart;
+// import javafx.stage.Stage;
+
+import java.util.ArrayList;
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.chart.LineChart;
+// import javafx.scene.chart.NumberAxis;
+// import javafx.scene.chart.XYChart;
+// import javafx.stage.Stage;
+
 // MAin class 
 class Karatsuba {
 
@@ -12,6 +30,11 @@ class Karatsuba {
     static long countComparisons = 0;
     static long countAssignments = 0;
     static long countMethodCalls = 0;
+
+    static long countTotalOperations = 0; // For total primitive operations count in the program
+
+    static ArrayList<Integer> digitsList = new ArrayList<>();
+    static ArrayList<Long> operationsList = new ArrayList<>();
 
     // Main driver method
     public static long mult(long x, long y) {
@@ -79,6 +102,11 @@ class Karatsuba {
         countMethodCalls++; // Another Math.pow call(Not sure)
 
         countMethodCalls++; // For the return statement
+
+        // Returning the final count
+        countTotalOperations = countAdditions + countMultiplications + countComparisons + countAssignments
+                + countMethodCalls;
+
         return ans;
 
     }
@@ -102,84 +130,241 @@ class Karatsuba {
         return noLen;
     }
 
+    // // Method 3
+    // // To write the results to a CSV file
+    // public static void writeToCSV(int nDigits, long totalOperations) {
+    // try {
+    // FileWriter csvWriter = new FileWriter("data.csv", true);
+    // csvWriter.append(nDigits + "," + totalOperations + "\n");
+    // csvWriter.flush();
+    // csvWriter.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // }
+
+    // // Method 3
+    // // To write the results to a CSV file
+    // public static void writeToCSV(int nDigits, long totalOperations, int a, int
+    // b) {
+    // try {
+    // FileWriter csvWriter = new FileWriter("data.csv", true);
+    // csvWriter.append(nDigits + "," + totalOperations + a + "," + b + "," + "\n");
+    // csvWriter.flush();
+    // csvWriter.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // }
+
+    // Method 3
+    // To write the results to a CSV file
+    public static void writeToCSV(int nDigits, long totalOperations, int x, int y) {
+        try {
+            FileWriter csvWriter = new FileWriter("data.csv", true);
+            csvWriter.append(nDigits + "," + totalOperations + "," + x + "," + y + "\n");
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // // Method 2
+    // // Main driver function
+    // public static void main(String[] args) {
+    // // Showcasing karatsuba multiplication
+
+    // try {
+    // FileWriter csvWriter = new FileWriter("data.csv");
+    // csvWriter.append("RandomNumber,TotalOperations\n");
+
+    // // // Case 1: Big integer lengths
+    // long expectedProduct = 1234 * 5678;
+    // long actualProduct = mult(1234, 5678);
+
+    // // Printing the expected and corresponding actual product
+    // System.out.println("Expected 1 : " + expectedProduct);
+    // System.out.println("Actual 1 : " + actualProduct + "\n\n");
+
+    // assert (expectedProduct == actualProduct);
+
+    // expectedProduct = 102 * 313;
+    // actualProduct = mult(102, 313);
+
+    // System.out.println("Expected 2 : " + expectedProduct);
+    // System.out.println("Actual 2 : " + actualProduct + "\n\n");
+
+    // assert (expectedProduct == actualProduct);
+
+    // expectedProduct = 1345 * 63456;
+    // actualProduct = mult(1345, 63456);
+
+    // System.out.println("Expected 3 : " + expectedProduct);
+    // System.out.println("Actual 3 : " + actualProduct + "\n\n");
+
+    // assert (expectedProduct == actualProduct);
+
+    // Integer x = null;
+    // Integer y = null;
+    // // Integer MAX_VALUE = 10000;
+    // Integer MAX_VALUE = 100; // For testing purposes
+
+    // // Boe creating an object of random class
+    // // inside main() method
+    // Random r = new Random();
+
+    // for (int i = 0; i < MAX_VALUE; i++) {
+    // x = (int) r.nextInt(MAX_VALUE);
+    // y = (int) r.nextInt(MAX_VALUE);
+
+    // // Reset counters
+    // countAdditions = 0;
+    // countMultiplications = 0;
+    // countComparisons = 0;
+    // countAssignments = 0;
+    // countMethodCalls = 0;
+    // countTotalOperations = 0;
+
+    // expectedProduct = x * y;
+
+    // // if (i == 9999) {
+    // if (i == 99) {// For testing purposes
+
+    // // Prove assertions catch the bad stuff.
+    // expectedProduct = 1;
+    // }
+    // actualProduct = mult(x, y);
+
+    // // Again printing the expected and
+    // // corresponding actual product
+    // // System.out.println("Expected: " + expectedProduct);
+    // // System.out.println("Actual: " + actualProduct + "\n\n");
+
+    // // Printing expected and actual products along with the count of operations
+    // System.out.println("Test " + (i + 1) + ":");
+    // System.out.println("Expected: " + expectedProduct);
+    // System.out.println("Actual: " + actualProduct);
+    // // System.out.println("Operations count:");
+    // // System.out.println(" Additions: " + countAdditions);
+    // // System.out.println(" Multiplications: " + countMultiplications);
+    // // System.out.println(" Comparisons: " + countComparisons);
+    // // System.out.println(" Assignments: " + countAssignments);
+    // // System.out.println(" Method Calls: " + countMethodCalls + "\n");
+    // System.out.println("Total Operations: " + countTotalOperations + "\n");
+
+    // assert (expectedProduct == actualProduct);
+    // }
+    // csvWriter.flush();
+    // csvWriter.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // }
+
     // Method 2
     // Main driver function
     public static void main(String[] args) {
         // Showcasing karatsuba multiplication
 
-        // // Case 1: Big integer lengths
-        long expectedProduct = 1234 * 5678;
-        long actualProduct = mult(1234, 5678);
+        try {
+            FileWriter csvWriter = new FileWriter("data.csv");
+            csvWriter.append("NumberOfDigits,TotalOperations\n");
 
-        // // Printing the expected and corresponding actual product
-        // System.out.println("Expected 1 : " + expectedProduct);
-        // System.out.println("Actual 1 : " + actualProduct + "\n\n");
+            // Case 1: Big integer lengths
+            long expectedProduct = 1234 * 5678;
+            long actualProduct = mult(1234, 5678);
 
-        // assert (expectedProduct == actualProduct);
-
-        // expectedProduct = 102 * 313;
-        // actualProduct = mult(102, 313);
-
-        // System.out.println("Expected 2 : " + expectedProduct);
-        // System.out.println("Actual 2 : " + actualProduct + "\n\n");
-
-        // assert (expectedProduct == actualProduct);
-
-        // expectedProduct = 1345 * 63456;
-        // actualProduct = mult(1345, 63456);
-
-        // System.out.println("Expected 3 : " + expectedProduct);
-        // System.out.println("Actual 3 : " + actualProduct + "\n\n");
-
-        // assert (expectedProduct == actualProduct);
-
-        Integer x = null;
-        Integer y = null;
-        // Integer MAX_VALUE = 10000;
-        Integer MAX_VALUE = 100; // For testing purposes
-
-        // Boe creating an object of random class
-        // inside main() method
-        Random r = new Random();
-
-        for (int i = 0; i < MAX_VALUE; i++) {
-            x = (int) r.nextInt(MAX_VALUE);
-            y = (int) r.nextInt(MAX_VALUE);
-
-            // Reset counters
-            countAdditions = 0;
-            countMultiplications = 0;
-            countComparisons = 0;
-            countAssignments = 0;
-            countMethodCalls = 0;
-
-            expectedProduct = x * y;
-
-            // if (i == 9999) {
-            if (i == 99) {// For testing purposes
-
-                // Prove assertions catch the bad stuff.
-                expectedProduct = 1;
-            }
-            actualProduct = mult(x, y);
-
-            // Again printing the expected and
-            // corresponding actual product
-            // System.out.println("Expected: " + expectedProduct);
-            // System.out.println("Actual: " + actualProduct + "\n\n");
-
-            // Printing expected and actual products along with the count of operations
-            System.out.println("Test " + (i + 1) + ":");
-            System.out.println("Expected: " + expectedProduct);
-            System.out.println("Actual: " + actualProduct);
-            System.out.println("Operations count:");
-            System.out.println("  Additions: " + countAdditions);
-            System.out.println("  Multiplications: " + countMultiplications);
-            System.out.println("  Comparisons: " + countComparisons);
-            System.out.println("  Assignments: " + countAssignments);
-            System.out.println("  Method Calls: " + countMethodCalls + "\n");
+            // Printing the expected and corresponding actual product
+            System.out.println("Expected 1 : " + expectedProduct);
+            System.out.println("Actual 1 : " + actualProduct + "\n\n");
 
             assert (expectedProduct == actualProduct);
+
+            expectedProduct = 102 * 313;
+            actualProduct = mult(102, 313);
+
+            System.out.println("Expected 2 : " + expectedProduct);
+            System.out.println("Actual 2 : " + actualProduct + "\n\n");
+
+            assert (expectedProduct == actualProduct);
+
+            expectedProduct = 1345 * 63456;
+            actualProduct = mult(1345, 63456);
+
+            System.out.println("Expected 3 : " + expectedProduct);
+            System.out.println("Actual 3 : " + actualProduct + "\n\n");
+
+            assert (expectedProduct == actualProduct);
+
+            Integer x = null;
+            Integer y = null;
+            String xString = null;
+            String yString = null;
+
+            // Integer MAX_VALUE = 10000;
+            Integer MAX_VALUE = 10000; // For testing purposes
+
+            // Boe creating an object of random class
+            // inside main() method
+            Random r = new Random(42);
+
+            for (int i = 0; i < MAX_VALUE; i++) {
+                x = (int) r.nextInt(MAX_VALUE);
+                y = (int) r.nextInt(MAX_VALUE);
+                xString = x.toString();
+                yString = y.toString();
+
+                while (xString.length() != yString.length()) {
+                    // // Generate random strings
+                    x = (int) r.nextInt(MAX_VALUE);
+                    y = (int) r.nextInt(MAX_VALUE);
+                    xString = x.toString();
+                    yString = y.toString();
+                    // x = Integer.toString(r.nextInt(MAX_VALUE));
+                    // y = Integer.toString(r.nextInt(MAX_VALUE));
+                }
+
+                // Reset counters
+                countAdditions = 0;
+                countMultiplications = 0;
+                countComparisons = 0;
+                countAssignments = 0;
+                countMethodCalls = 0;
+                countTotalOperations = 0;
+
+                expectedProduct = x * y;
+
+                // if (i == 9999) {
+                if (i == 9999) {// For testing purposes
+
+                    // Prove assertions catch the bad stuff.
+                    expectedProduct = 1;
+                }
+                actualProduct = mult(x, y);
+
+                // Again printing the expected and
+                // corresponding actual product
+                // System.out.println("Expected: " + expectedProduct);
+                // System.out.println("Actual: " + actualProduct + "\n\n");
+
+                // Printing expected and actual products along with the count of operations
+                System.out.println("Test " + (i + 1) + ":");
+                System.out.println("Expected: " + expectedProduct);
+                System.out.println("Actual: " + actualProduct);
+                System.out.println("Total Operations: " + countTotalOperations + "\n");
+
+                // Write the results to the CSV file
+                writeToCSV(String.valueOf(x).length(), countTotalOperations, x, y);
+                // writeToCSV(numLength(x), countTotalOperations, x, y);
+                // writeToCSV(numLength(x), countTotalOperations, x, y);
+
+                assert (expectedProduct == actualProduct);
+            }
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
